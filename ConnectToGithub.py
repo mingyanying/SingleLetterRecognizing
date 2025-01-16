37f3f2cc111cffa0ff21f91394f4d402f77d683e
Mingyan Ying <Mingyan Ying>
2025-01-16 12:22:09 -0600

    1/16/2025

---
+++ ConnectToGithub.py
@@ -0,0 +1,22 @@
+'''
+# 1. Clone the GitHub repository
+git clone https://github.com/mingyanying/SingleLetterRecognizing.git
+
+# 2. Navigate to the cloned repository
+cd SingleLetterRecognizing
+
+# 3. Initialize Git if needed
+git init
+
+# 4. Set the remote repository (if not already set)
+git remote add origin https://github.com/mingyanying/SingleLetterRecognizing.git
+
+# 5. Stage all changes
+git add .
+
+# 6. Commit changes with a message
+git commit -m "Your commit message"
+
+# 7. Push the changes to GitHub
+git push origin main
+'''
